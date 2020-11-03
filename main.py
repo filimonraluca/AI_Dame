6651f6a9fbe498e49cdd10991c01d4f4e20e19fd
Filimon Raluca <raluca.filimon@info.uaic.ro>
2020-11-03 09:52:15 +0200

    Cu remiza

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 from termcolor import colored
 
+
 class Game:
     def __init__(self):
-        self.board = [[1, 1, 1, 1],
-                      [0, 0, 0, 0],
-                      [0, 0, 0, 0],
-                      [2, 2, 2, 2]]
         self.human_poz = [(3, 0), (3, 1), (3, 2), (3, 3)]
         self.ai_poz = [(0, 0), (0, 1), (0, 2), (0, 3)]
         self.turn = 2
+        self.game_over = False
 
     def is_final(self):
-        if self.board[0] == [2, 2, 2, 2]:
+        h_result = len(list(filter(lambda e: e[0] == 0, self.human_poz)))
+        ai_result = len(list(filter(lambda e: e[0] == 3, self.ai_poz)))
+        if self.game_over == True:
+            if h_result == ai_result:
+                print("Nobody wins")
+                return -1
+            elif h_result > ai_result:
+                print("Human wins! :)")
+                return 2
+            else:
+                print("AI wins! :(")
+                return 1
+        if h_result == 4:
             print("Human wins! :)")
             return 2
-        if self.board[3] == [1, 1, 1, 1]:
+        if ai_result == 4:
             print("AI wins! :(")
             return 1
         return 0
 
     def is_valid(self, l, c):
-        if 0 <= l < 4 and 0 <= c < 4 and self.board[l][c] == 0:
+        if 0 <= l < 4 and 0 <= c < 4 and (l, c) not in self.ai_poz and (l, c) not in self.human_poz:
             return True
         return False
 
     def is_human_piece(self, l, c):
-        if 0 <= l < 4 and 0 <= c < 4 and self.board[l][c] == 2:
+        if 0 <= l < 4 and 0 <= c < 4 and (l, c) in self.human_poz:
             return True
         return False
 
@@ -45,57 +55,67 @@ class Game:
         return f
 
     def make_ai_move(self, oldl, oldc, l, c):
-        self.board[oldl][oldc] = 0
         self.ai_poz[self.ai_poz.index((oldl, oldc))] = (l, c)
-        self.board[l][c] = 1
 
     def make_human_move(self, oldl, oldc, l, c):
-        self.board[oldl][oldc] = 0
         self.human_poz[self.human_poz.index((oldl, oldc))] = (l, c)
-        self.board[l][c] = 2
 
     def next_move(self):
         l = [-1, -1, -1, 0, 0, 1, 1, 1]
         c = [-1, 0, 1, -1, 1, -1, 0, 1]
         mx = -10
         best_move = None
+        valid_moves = 0
         if self.turn == 1:
             for piece in self.ai_poz:
                 for i in range(0, 8):
                     if self.is_valid(piece[0] + l[i], piece[1] + c[i]):
+                        valid_moves += 1
                         eval_result = self.euristic_function(piece, piece[0] + l[i], piece[1] + c[i])
                         if eval_result > mx:
                             mx = eval_result
                             best_move = (piece[0], piece[1], piece[0] + l[i], piece[1] + c[i])
-            self.make_ai_move(best_move[0], best_move[1], best_move[2], best_move[3])
-            self.turn = 2
+            if valid_moves == 0:
+                self.game_over = True
+            else:
+                self.make_ai_move(best_move[0], best_move[1], best_move[2], best_move[3])
+                self.turn = 2
         elif self.turn == 2:
-            x, y = map(int, input("Enter x and y for piece you want to move:").split())
-            while not self.is_human_piece(x, y):
-                x, y = map(int, input("Enter VALID x and y for piece you want to move:").split())
-            next_x, next_y = map(int, input("Enter x and y for new position:").split())
-            while not self.is_valid(next_x, next_y):
-                next_x, next_y = map(int, input("Enter VALID x and y for new position:").split())
-            while not self.is_logical_move(x, y, next_x, next_y):
-                print("The move you want to make is forbidden! (too far)")
+            for piece in self.human_poz:
+                for i in range(0, 8):
+                    if self.is_valid(piece[0] + l[i], piece[1] + c[i]):
+                        valid_moves += 1
+            if valid_moves == 0:
+                self.game_over = True
+            else:
                 x, y = map(int, input("Enter x and y for piece you want to move:").split())
                 while not self.is_human_piece(x, y):
                     x, y = map(int, input("Enter VALID x and y for piece you want to move:").split())
                 next_x, next_y = map(int, input("Enter x and y for new position:").split())
                 while not self.is_valid(next_x, next_y):
                     next_x, next_y = map(int, input("Enter VALID x and y for new position:").split())
-            self.make_human_move(x, y, next_x, next_y)
-            self.turn = 1
+                while not self.is_logical_move(x, y, next_x, next_y):
+                    print("The move you want to make is forbidden! (too far)")
+                    x, y = map(int, input("Enter x and y for piece you want to move:").split())
+                    while not self.is_human_piece(x, y):
+                        x, y = map(int, input("Enter VALID x and y for piece you want to move:").split())
+                    next_x, next_y = map(int, input("Enter x and y for new position:").split())
+                    while not self.is_valid(next_x, next_y):
+                        next_x, next_y = map(int, input("Enter VALID x and y for new position:").split())
+                self.make_human_move(x, y, next_x, next_y)
+                self.turn = 1
 
     def print_board(self):
         print("Starea curenta:")
-        for l in self.board:
-            for c in l:
-                if c==1: print (colored(c, 'red'),end=' ')
-                elif c == 2: print(colored(c, 'green'),end=' ')
-                else: print(colored(c, 'grey'),end=' ')
+        for i in range(0, 4):
+            for j in range(0, 4):
+                if (i, j) in self.human_poz:
+                    print(colored('H', 'green'), end=' ')
+                elif (i, j) in self.ai_poz:
+                    print(colored('C', 'red'), end=' ')
+                else:
+                    print(colored(0, 'grey'), end=' ')
             print()
-
 
 
 if __name__ == '__main__':
